9482c625c9e6de1e5b52147c2f131ecca1e47df3
Rodrigo Norys <rodrigo.norys@gmail.com>
2025-08-27 11:36:03 -0300

    Noite de somo = Melhora no código

---
+++ passwordGenerator/readme.txt
@@ -0,0 +1,7 @@
+Quantidade de chars.
+Se adiciona números.
+Se adiciona letras maiúsculas.
+Se adiciona letras minúsculas.
+Símbolos
+
+Chamar as funções para serem executadas no evento do click do botão.
--- passwordGenerator/src/modules/PasswordGenerator.js
+++ passwordGenerator/src/modules/PasswordGenerator.js
@@ -4,21 +4,16 @@ export default class PasswordGenerator {
         this.charLimit = document.getElementById("char-limit");
         this.generateButton = document.getElementById("generate-button");
         this.checkBoxes = document.querySelectorAll('.container input[type="checkbox"]');
-        this.element0 = false;
-        this.element1 = false;
-        this.element2 = false;
-        this.element3 = false;
     }
 
     init() {
         this.genButton();
-        this.change();
     }
-    
+
     genButton() {
-        this.generateButton.addEventListener("click", event => {  
-            this.paragrafo();
-            this.teste();
+        this.generateButton.addEventListener("click", event => {
+            this.generatedPassword();
+            this.passwordBuilder();
         });
     };
 
@@ -26,29 +21,22 @@ export default class PasswordGenerator {
         return Math.floor(Math.random() * (max - min)) + min;
     }
 
-    paragrafo() {
-        return this.yourPassword.innerText = this.teste();
+    generatedPassword() {
+        return this.yourPassword.innerText = this.passwordBuilder();
     }
 
-    change() {
-        this.checkBoxes[0].addEventListener("change", event => { this.element0 = event.target.checked; });
-        this.checkBoxes[1].addEventListener("change", event => { this.element1 = event.target.checked; });
-        this.checkBoxes[2].addEventListener("change", event => { this.element2 = event.target.checked; });
-        this.checkBoxes[3].addEventListener("change", event => { this.element3 = event.target.checked; });
-    }
-
-    teste() {
-        let alo = "";
+    passwordBuilder() {
+        let password = "";
         const symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
         for (let i = 0; i < this.charLimit.value; i++) {
-            if (this.element0 === true) alo += String.fromCharCode(this.rand(48, 57));
-            if (this.element1 === true) alo += String.fromCharCode(this.rand(65, 90));
-            if (this.element2 === true) alo += String.fromCharCode(this.rand(97, 122));
-            if (this.element3 === true) {
-                alo += symbols[this.rand(0, symbols.length - 1)];
+            if (this.checkBoxes[0].checked) password += String.fromCharCode(this.rand(48, 57));  // Numbers
+            if (this.checkBoxes[1].checked) password += String.fromCharCode(this.rand(65, 90));  // Lowercase
+            if (this.checkBoxes[2].checked) password += String.fromCharCode(this.rand(97, 122)); // Uppercase
+            if (this.checkBoxes[3].checked) {
+                password += symbols[this.rand(0, symbols.length - 1)];
             }
         }
- 
-        return alo.slice(0, this.charLimit.value)
+
+        return password.slice(0, this.charLimit.value)
     }
 }
