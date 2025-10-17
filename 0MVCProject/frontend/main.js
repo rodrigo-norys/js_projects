4ca55b56b1327f419a9bba0874ded13bb498aca2
Rodrigo Norys <otaviomirandabr@gmail.com>
2025-10-17 11:23:05 -0300

    Alterações

--- 0MVCProject/frontend/main.js
+++ 0MVCProject/frontend/main.js
@@ -1,5 +1,5 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
 
 import Auth from './validators/Auth.js';
 import Contact from './validators/Contact.js';
