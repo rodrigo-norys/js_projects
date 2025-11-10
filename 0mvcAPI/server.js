bb67d50f2607251826e9949b6341ed7721b48d65
Rodrigo Freitas Norys <rodrigo.norys@gmail.com>
2025-11-10 11:28:40 -0300

    Delete 0mvcAPI directory

--- 0mvcAPI/server.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-
-const port = 3001;
-app.listen(port, () => {
-  console.log();
-  console.log(`Escutando na porta ${port}`);
-});
--- 0mvcAPI/src/routes/tokenRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import tokenController from '../controllers/TokenController.js';
-
-const router = new Router();
-
-router.post('/', tokenController.create);
-
-export default router;
